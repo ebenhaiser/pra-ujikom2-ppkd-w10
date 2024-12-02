501e95c01de2effadb37b913bb4de5e61ba0f9f9
nasywasyifa <ebenhaiser@gmail.com>
2024-12-02 09:54:03 +0700

    update footer

--- admin/inc/footer.php
+++ admin/inc/footer.php
@@ -1,7 +1,12 @@
 <footer class="content-footer footer bg-footer-theme">
     <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
-        <div class="mx-auto">
-            <!-- nama -->
+        <div class="mx-auto d-flex gap-3">
+            <!-- &#169; Laundry Faith | by Ebenhaiser | Using Sneat Template -->
+            <span>&#169; Laundry Faith</span>
+            |
+            <span>by Ebenhaiser</span>
+            |
+            <span>Using Sneat Template</span>
         </div>
     </div>
 </footer>
